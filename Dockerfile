16394ccf7d60b12fc7670e391750ec6800f3cfa7
Sophie <sophie.cooper@7digital.com>
2020-04-27 15:12:00 +0100

    Install google chrome in image

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
 FROM node:slim
 
 LABEL maintainer=Sophie
+
+RUN \
+  apt-get update -qq && apt-get -qqy --no-install-recommends install \
+    curl ca-certificates gnupg2 && \
+\
+  echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' \
+    >> /etc/apt/sources.list && \
+  curl -s https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+  apt-get update -qq && apt-get -qqy --no-install-recommends install google-chrome-stable
--- test/spec/dockerfile_spec.rb
+++ test/spec/dockerfile_spec.rb
@@ -6,6 +6,9 @@ require 'docker'
 describe 'scenario test Dockerfile' do
   before(:all) do
     @image = Docker::Image.build_from_dir('../scenarios')
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_image, @image.id
   end
 
   it 'has the maintainer label' do
@@ -13,5 +16,9 @@ describe 'scenario test Dockerfile' do
 
     expect(labels.key?('maintainer')).to be true
     expect(labels['maintainer']).to eq('Sophie')
+  end
+
+  describe package('google-chrome-stable') do
+    it { should be_installed }
   end
 end
